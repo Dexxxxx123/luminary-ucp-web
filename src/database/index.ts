58183a4f136c1f7de145fd197f5b0ba5fccf8309
Dexxxxx123 <kecedexxx@gmail.com>
2025-02-28 05:51:54 +0800

    Update index.ts

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,8 +1,8 @@
 import mysql from "mysql2";
 
 export const connection = mysql.createConnection({
-  host: "204.10.192.68",
-  user: "u259_s3P9NatR6y",
-  password: "63PoBX.vcOQ!65Z1uNQDt4.l",
-  database: "s259_luminary_samp",
+  host: "178.248.73.13",
+  user: "u1_RxqIsahVI5",
+  password: "akfpXZp8k5vc^eVj5!2n.FVL",
+  database: "s1_lhrp",
 });
